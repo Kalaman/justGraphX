fb002dbd0d891f8fd28440b1af65db01e1d44dd2
Kalaman <asd>
2017-01-16 20:31:01 +0100

    GUI added

---
+++ src/Graph/Utilities/Constants.java
@@ -0,0 +1,24 @@
+package Graph.Utilities;
+
+/**
+ * Created by Jok3r on 13.01.2017.
+ */
+public class Constants {
+
+    public static final int SIZE_MULTIPLIKATOR = 8;
+
+    public static final int FRAME_SIZE_X = 150 * SIZE_MULTIPLIKATOR;
+    public static final int FRAME_SIZE_Y = 100 * SIZE_MULTIPLIKATOR;
+
+    public static final int PANEL_LEFT_MENU_SIZE_X = 40 * SIZE_MULTIPLIKATOR;
+    public static final int PANEL_LEFT_MENU_SIZE_Y = FRAME_SIZE_Y;
+
+    public static final int PANEL_GRAPH_SIZE_X = FRAME_SIZE_X - PANEL_LEFT_MENU_SIZE_X;
+    public static final int PANEL_GRAPH_SIZE_Y = FRAME_SIZE_Y;
+
+    public static final int PANEL_STATUS_BAR_SIZE_X = FRAME_SIZE_X;
+    public static final int PANEL_STATUS_BAR_SIZE_Y = 4 * SIZE_MULTIPLIKATOR;
+
+    public static final int NODE_WIDTH = 25;
+    public static final int NODE_HEIGHT = NODE_WIDTH;
+}
